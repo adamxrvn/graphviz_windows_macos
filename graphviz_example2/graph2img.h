a185e6823555222df6cb7ba2e8c8c3b61b939c5f
WhiteBlackGoose <wbg@angouri.org>
2022-02-08 17:35:50 +0300

    Update graph2img.h

--- graphviz_example2/graph2img.h
+++ graphviz_example2/graph2img.h
@@ -14,54 +14,56 @@
 
 // Creates a DOT file, representing the graph, which can be used as input for visualization in Graphviz
 template<typename T>
-void printImage(std::string fileName, const char *extension, Graph<T> &graph) {
+void printImage(std::string fileName, const char* extension, Graph<T>& graph, bool ignoreDoubleEdges = false) {
     using Node = typename Graph<T>::Node;
 
-    Agraph_t *g;                            // Graphviz's special graph data structure
-    std::map<Node *, Agnode_t *> agnodes;      // A mapping between nodes of our graph to nodes in the Graphviz's graph
+    Agraph_t* g;                            // Graphviz's special graph data structure
+    std::map<Node*, Agnode_t*> agnodes;      // A mapping between nodes of our graph to nodes in the Graphviz's graph
 
     // Setting up a Graphviz Context - but only once even for multiple graphs
 #ifndef NO_LAYOUT_OR_RENDERING
-    static GVC_t *gvc;
+    static GVC_t* gvc;
     if (!gvc)
         gvc = gvContext();
 #endif
 
-    g = agopen("g", Agundirected, NULL); // Change constant to "Agdirected" for directed graphs
+    g = agopen((char*)"g", Agundirected, NULL); // Change constant to "Agdirected" for directed graphs
 
-    for (Node *node: graph.getNodes()) {
+    for (Node* node : graph.getNodes()) {
         std::stringstream ss;
         ss << node->data;
         char nodeLabel[20];
         ss >> nodeLabel;
 
         agnodes[node] = agnode(g, nodeLabel,
-                               TRUE);  // 'nodeLabel' will also internally denote the node identifier in the graph
+            TRUE);  // 'nodeLabel' will also internally denote the node identifier in the graph
 
-        // Setting attributes when creating the nodes
-        agsafeset(agnodes[node], "color", "red", "");
+// Setting attributes when creating the nodes
+        agsafeset(agnodes[node], (char*)"color", "red", "");
     }
 
-    bool skip = true;
+    std::set<std::pair<Node*, Node*>> alreadyAddedEdges;
 
-    for (auto edge: graph.getEdgeWeights()) {
-        if ((skip = !skip)) // only for undirected graphs
+    for (auto [ pair, weight ] : graph.getEdgeWeights()) {
+        
+        auto [u, v] = pair;
+
+        if (ignoreDoubleEdges && (alreadyAddedEdges.contains(std::make_pair(u, v)) || alreadyAddedEdges.contains(std::make_pair(v, u))))
             continue;
-
-        int weight = edge.first;
-        Node *u = edge.second.first;
-        Node *v = edge.second.second;
-
+        
+        if (ignoreDoubleEdges)
+            alreadyAddedEdges.insert(std::make_pair(u, v));
+        
         std::stringstream ss;
         ss << weight;
         char edgeWeight[20];
         ss >> edgeWeight;
 
-        Agedge_t *ghEdge = agedge(g, agnodes[u], agnodes[v], nullptr, TRUE);
+        Agedge_t* ghEdge = agedge(g, agnodes[u], agnodes[v], nullptr, TRUE);
 
         // Setting attributes when creating the edges
-        agsafeset(ghEdge, "color", "blue", "");
-        agsafeset(ghEdge, "label", edgeWeight, "");
+        agsafeset(ghEdge, (char*)"color", "blue", "");
+        agsafeset(ghEdge, (char*)"label", edgeWeight, "");
     }
 
 #ifdef NO_LAYOUT_OR_RENDERING
